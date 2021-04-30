ea4460506b4b6f6e3d3b5d3ff9a0a6e06fc92a17
giulio524 <62382801+giulio524@users.noreply.github.com>
2021-04-30 16:52:54 +0200

    Preprocessing lingua
    Ho fatto la parte del preprocessing sulla lingua, ho cercato di fare language detection tramite un algoritmo non molto preciso ma veloce.
    
    Volevo confrontare i risultati di questo con l'api di google ma ho esaurito le richieste diponibili, riproverò in seguito.

--- Mining.py
+++ Mining.py
@@ -17,16 +17,132 @@ for i in range(0, df.text.size):
 
 df['text_enc'] = text_enc
 
-#PROVO A FARE UN PO DI LANGUAGE DETECTION
+
+
+#PROVO A FARE UN PO DI LANGUAGE DETECTION---------------------------------------------------------------------------
+
 from langdetect import detect_langs
+import re
+
 #il metodo detect_langs fornisce un vettore di possibilità riguardo alla lingua del testo che sta analizzando
 #vado a vedere se ci sono nel dataset testi ambigui (quindi quelli per cui la dimensione del dict ritornato da detect_langs)
 #è maggiore di 1
+
+# utilizzo langdetect che è abbastanza veloce per generare una previsione sommaria. Esso restituisce la probabilità per ciascuna lingua
+# trovata quindi:
+#   - se trovo una sola lingua tra quelle ammesse (en, es, fr) allora segno la lingua trovata e segno la detection come sicura
+#   - se trovo più di una lingua oppure trovo come lingua più probabile una non ammessa segno la lingua e segno la detection come insicura/sbagliata 
+
+unsure_count = 0
+wrong_count = 0
+i=0
+lang_detect_before = []
+unsure_wrong_before = []
 for twt in df.text_enc:
-    detection = detect_langs(twt)
-    if(len(detection)>1):
-        print(twt)
-        print(detection)
+    try:
+        detection = detect_langs(twt)
+        lang = detection[0].lang
+        
+        lang_detect_before.append(lang)
+        unsure_wrong_before.append(False)
+        
+        if((lang != "en") and (lang != "es") and (lang != "fr")):
+             wrong_count = wrong_count +1
+             unsure_wrong_before[i]=True
+        if(len(detection)>1):
+            unsure_wrong_before[i]=True
+            unsure_count = unsure_count+1
+
+    except:
+        print("errore alla posizione ")
+        i
+        print("testo : " + twt)
+        lang_detect_before.append('None')
+        unsure_wrong_before.append(True)
+
+    i=i+1
+
+
+
+df['langdetect']=lang_detect
+df['unsure_wrong_detection'] = unsure_wrong
+
+unsure_wrong_before_sum = df['unsure_wrong_detection'].sum()
+unsure_wrong_before_sum
+
+#PREVENTIVO AL LANGUAGE DETECTION: PULIZIA DEI TWEET
+# vado a vedere se la pulizia dei tweet migliora le performance della traduzione
+
+def cleanTxt(text):
+ text = re.sub('@[A-Za-z0–9]+', '', text) #Rimuove le @menzioni
+ text = re.sub('#', '', text) # Rimuove l'hashtag
+ text = re.sub('https?:\/\/\S+', '', text) # Rimuove i link
+ return text
+
+
+df['text_clean'] = df.text_enc.apply(cleanTxt)
+
+#--------performo di nuovo la traduzione
+unsure_count = 0
+wrong_count = 0
+i=0
+lang_detect = []
+unsure_wrong = []
+for twt in df.text_clean:
+    try:
+        detection = detect_langs(twt)
+        lang = detection[0].lang
+        
+        lang_detect.append(lang)
+        unsure_wrong.append(False)
+        
+        if((lang != "en") and (lang != "es") and (lang != "fr")):
+             wrong_count = wrong_count +1
+             unsure_wrong[i]=True
+        if(len(detection)>1):
+            unsure_wrong[i]=True
+            unsure_count = unsure_count+1
+    except:
+        print("errore alla posizione ")
+        i
+        print("testo : " + twt)
+        lang_detect.append('None')
+        unsure_wrong.append(True)
+
+    i=i+1
+
+
+df['langdetect']=lang_detect
+df['unsure_wrong_detection'] = unsure_wrong
+
+unsure_wrong_after_sum = df['unsure_wrong_detection'].sum()
+unsure_wrong_after_sum
+
+#pulire il testo migliora di pochissimo (quasi niente) le performance della language detection
+
+#CHECK DELLA TRADUZIONE CON API GOOGLE
+#Questa API non è stata utilizzata prima perché estremamente lenta e non permette di effettuare troppe richieste ai server google
+
+from textblob import TextBlob
+
+lang_detect_final = []
+i = 0
+for twt in df.text_clean:
+    if not df['unsure_wrong_detection'][i]:
+        lang_detect_final.append(df['langdetect'][i])
+    else:
+        blob = TextBlob(twt)
+        lang = blob.detect_language()
+        if((lang != "en") and (lang != "es") and (lang != "fr")):
+            lang_detect_final.append('None')
+            print("nesun match alla riga")
+            i
+        else:
+            lang_detect_final.append(lang)
+    i=i+1
+
+df['lang_detect_final'] = lang_detect_final
+
 
 
 #ESPORTO I DATI NUOVI IN EXCEL PER ANALIZZARLI SU RAPID MINER
